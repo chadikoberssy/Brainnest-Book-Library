7595b3287f232f49b54405f91f1de0318607abdb
KRN <67783847+KRN-8@users.noreply.github.com>
2023-04-29 19:38:06 +0200

    semiFinished JS

--- scripts/BookLibrary.js
+++ scripts/BookLibrary.js
@@ -18,6 +18,45 @@ class Book {
 	}
 }
 
-function addBook(book = new Book("RNG book", "John Doe", Math.round(Math.random()*1337))) {
+function addBook(book = new Book("RNG book", "John Doe", Math.round(Math.random() * 1337))) {
 	bookLibrary.push(book);
+	update();
+}
+
+function update() {
+	const bookShelf = document.querySelector("#bookshelf");
+	bookShelf.textContent = "";
+
+	bookLibrary.forEach((i) => {
+		const book = document.createElement("tr");
+		book.classList.add("books");
+		bookShelf.appendChild(book);
+
+		const bookTitle = document.createElement("td");
+		bookTitle.textContent = i.name;
+		book.appendChild(bookTitle);
+
+		const bookAuthor = document.createElement("td");
+		bookAuthor.textContent = i.author;
+		book.appendChild(bookAuthor);
+
+		const bookPages = document.createElement("td");
+		bookPages.textContent = i.pageCount;
+		book.appendChild(bookPages);
+
+		const statusSymbol = document.createElement("a");
+		statusSymbol.classList.add(i.read ? "" : "not-" + "read");
+		statusSymbol.innerText = i.read ? "Did" : "Did NOT" + " read";
+		const bookStatus = document.createElement("td");
+		bookStatus.appendChild(statusSymbol);
+		book.appendChild(bookStatus);
+
+		// Remove "book" button
+		const deleteSymbol = document.createElement("a");
+		deleteSymbol.classList.add("trash");
+		deleteSymbol.innerText = "X";
+		const bookDelete = document.createElement("td");
+		bookDelete.appendChild(deleteSymbol);
+		book.appendChild(bookDelete);
+	});
 }
