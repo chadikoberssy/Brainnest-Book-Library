eeaefa7d662c0be086c040b6a81765997346c7d9
Maria Sharova <sharova.maria.3d@gmail.com>
2023-05-01 17:23:22 +0200

    added the new logic of new book creation with new html

--- scripts/BookLibrary.js
+++ scripts/BookLibrary.js
@@ -23,36 +23,72 @@ function reDraw() {
 	bookShelf.textContent = "";
 
 	bookLibrary.forEach((i) => {
-		const book = document.createElement("tr");
-		book.classList.add("books");
+		// WHOLE BOOK CREATION
+		const book = document.createElement("table");
+		book.classList.add("bookshelf-table");
 		bookShelf.appendChild(book);
 
-		const bookTitle = document.createElement("td");
+		// BOOK HEADER CREATION
+		const tableHeader = document.createElement("thead");
+		tableHeader.classList.add("book-header");
+		book.appendChild(tableHeader);
+
+		const tableHeaderRow = document.createElement("tr");
+		tableHeaderRow.classList.add("book-header-row");
+		tableHeader.appendChild(tableHeaderRow);
+
+		const bookTitle = document.createElement("th");
 		bookTitle.textContent = i.name;
-		book.appendChild(bookTitle);
+		tableHeaderRow.appendChild(bookTitle);
+
+		// BOOK BODY CREATION
+		const tableBody = document.createElement("tbody");
+		tableBody.classList.add("book-body");
+		book.appendChild(tableBody);
+
+		// BOOK BODY AUTHOR SECTION CREATION
+		const tableBodyRowAuthor = document.createElement("tr");
+		tableBodyRowAuthor.classList.add("book-body-row-author");
+		tableBody.appendChild(tableBodyRowAuthor);
 
 		const bookAuthor = document.createElement("td");
 		bookAuthor.textContent = i.author;
-		book.appendChild(bookAuthor);
+		tableBodyRowAuthor.appendChild(bookAuthor);
+
+		// BOOK BODY PAGES SECTION CREATION
+		const tableBodyRowPages = document.createElement("tr");
+		tableBodyRowPages.classList.add("book-body-row-pages");
+		tableBody.appendChild(tableBodyRowPages);
 
 		const bookPages = document.createElement("td");
 		bookPages.textContent = i.pageCount;
-		book.appendChild(bookPages);
+		tableBodyRowPages.appendChild(bookPages);
 
-		const statusSymbol = document.createElement("a");
+		// BOOK BODY STATUS SECTION CREATION
+		const tableBodyRowStatus = document.createElement("tr");
+		tableBodyRowStatus.classList.add("book-body-row-status");
+		tableBody.appendChild(tableBodyRowStatus);
+
+		const statusSymbol = document.createElement("button");
 		statusSymbol.classList.add(i.read ? "" : "not-" + "read");
 		statusSymbol.innerText = i.read ? "Did" : "Did NOT" + " read";
-		const bookStatus = document.createElement("td");
-		bookStatus.appendChild(statusSymbol);
-		book.appendChild(bookStatus);
 
-		// Remove "book" button
-		const deleteSymbol = document.createElement("a");
+		const tableBodyRowStatusData = document.createElement("td");
+		tableBodyRowStatusData.appendChild(statusSymbol);
+		tableBodyRowStatus.appendChild(tableBodyRowStatusData);
+
+		// BOOK BODY Remove "book" button SECTION
+		const tableBodyRowTrash = document.createElement("tr");
+		tableBodyRowTrash.classList("book-body-row-trash");
+		tableBody.appendChild(tableBodyRowTrash);
+
+		const deleteSymbol = document.createElement("button");
 		deleteSymbol.classList.add("trash");
 		deleteSymbol.innerText = "X";
-		const bookDelete = document.createElement("td");
-		bookDelete.appendChild(deleteSymbol);
-		book.appendChild(bookDelete);
+
+		const tableBodyRowStatusTrash = document.createElement("td");
+		tableBodyRowStatusTrash.appendChild(deleteSymbol);
+		tableBodyRowTrash.appendChild(tableBodyRowStatusTrash);
 	});
 }
 
