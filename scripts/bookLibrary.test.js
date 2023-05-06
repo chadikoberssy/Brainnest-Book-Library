3d4ebe7b18074db5bac065a3ee042f5889c36418
benibry <122222790+benibry@users.noreply.github.com>
2023-05-06 20:34:27 +0200

    lost capitalize and found it

--- scripts/bookLibrary.test.js
+++ scripts/bookLibrary.test.js
@@ -1,19 +1,40 @@
 
 const bookLibrary = require('./BookLibrary')
 
-
-
-test("addBook function should add a book object to bookLibrary array", () => {
-    const initialLength = bookLibrary.length;
-    const testName = "Test Book";
-    const testAuthor = "Test Author";
-    const testPageCount = 200;
-    const testRead = false;
-    addBook(testName, testAuthor, testPageCount, testRead);
-    expect(bookLibrary.length).toBe(initialLength + 1);
-    const lastBook = bookLibrary[bookLibrary.length - 1];
-    expect(lastBook.name).toBe(testName);
-    expect(lastBook.author).toBe(testAuthor);
-    expect(lastBook.pageCount).toBe(testPageCount);
-    expect(lastBook.read).toBe(testRead);
+describe("addBook", () => {
+    beforeEach(() => {
+      // Reset bookLibrary before each test
+      bookLibrary = [];
+    });
+  
+    test("should add a book to the library", () => {
+      const initialLength = bookLibrary.length;
+      addBook("Harry Potter", "J.K. Rowling", 400, true);
+      expect(bookLibrary.length).toBe(initialLength + 1);
+    });
+  
+    test("should add a book with correct properties", () => {
+      addBook("Harry Potter", "J.K. Rowling", 400, true);
+      const newBook = bookLibrary[0];
+      expect(newBook.name).toBe("Harry Potter");
+      expect(newBook.author).toBe("J.K. Rowling");
+      expect(newBook.pageCount).toBe(400);
+      expect(newBook.read).toBe(true);
+    });
   });
+
+
+// test("addBook function should add a book object to bookLibrary array", () => {
+//     const initialLength = bookLibrary.length;
+//     const testName = "Test Book";
+//     const testAuthor = "Test Author";
+//     const testPageCount = 200;
+//     const testRead = false;
+//     addBook(testName, testAuthor, testPageCount, testRead);
+//     expect(bookLibrary.length).toBe(initialLength + 1);
+//     const lastBook = bookLibrary[bookLibrary.length - 1];
+//     expect(lastBook.name).toBe(testName);
+//     expect(lastBook.author).toBe(testAuthor);
+//     expect(lastBook.pageCount).toBe(testPageCount);
+//     expect(lastBook.read).toBe(testRead);
+//   });
